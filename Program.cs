e531ba96c40e1d065c922ce6c99a2d0578a78b2a
kahlkritz <31761516+kahlkritz@users.noreply.github.com>
2019-07-13 15:25:12 +0300

    C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Attempt_4._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
